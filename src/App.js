de25cef16a61c81705a483e6cf04a7fdaeac58b9
OmarArias <omarxarias@gmail.com>
2021-09-28 22:46:45 -0400

    Aumentando tercera prueba, muestra el precio con descuento

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ function sumar(a, b) {
 }
 
 function totalizador(cantidad,precio){
-  return cantidad*precio
+  return descuento(cantidad*precio);
+}
+
+function descuento(cuenta){
+  let descuento = 0;
+  if(cuenta >= 1000){
+    descuento = cuenta*0.03;
+  }
+  if(cuenta >= 3000){
+    descuento = cuenta*0.05;
+  }
+  if(cuenta >= 7000){
+    descuento = cuenta*0.07;
+  }
+  if(cuenta >= 10000){
+    descuento = cuenta*0.1;
+  }
+  if(cuenta >= 30000){
+    descuento = cuenta*0.15;
+  }
+
+  let total = cuenta - descuento;
+  return total;
 }
 
 export {sumar, totalizador};
--- src/App.test.js
+++ src/App.test.js
@@ -10,5 +10,8 @@ describe("Sumar", () => {
   it("Deberia mostrar el precio de comprar X productos con Y costo", () => {
     expect(totalizador(3,5)).toEqual(15);
   });
+  it("Deberia mostrar el precio de comprar X productos con Y costo con descuento", () => {
+    expect(totalizador(10,100)).toEqual(970);
+  });
 });
 
