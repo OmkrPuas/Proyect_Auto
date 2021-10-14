08d3a2517642e23d01f8d22a75043a6f0995318c
OmarArias <omarxarias@gmail.com>
2021-10-13 21:29:41 -0400

    Commit 3, Reconocimiento de numeros separados por una coma o por el slash, y devuelve la primera parte como numeros

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ function crear_Matrix(tam_x){
 }
 
 function decifrarCadena(cadena){
-  var numeros = cadena.split(",")
-  for (var i=0; i < numeros.length; i++) {
-    numeros[i] = parseInt(numeros[i]);
- }
-  return numeros
+  var mensaje = cadena.split("/");
+  if (mensaje[0].length > 1){
+    var numeros_primera_parte = mensaje[0].split(",");
+    for (var i=0; i < numeros_primera_parte.length; i++) {
+      numeros_primera_parte[i] = parseInt(numeros_primera_parte[i]);
+    }
+    mensaje[0] = numeros_primera_parte;
+  }else{
+    mensaje[0] = parseInt(mensaje[0]);
+  }
+  
+  return mensaje
 }
 
 export {decifrarCadena, crear_Matrix};
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,10 @@ describe("Descifrar", () => {
     expect(decifrarCadena("5")).toEqual([5]);
   });
   it("deberia reconocer los numero de un string", () => {
-    expect(decifrarCadena("5,5")).toEqual([5,5]);
+    expect(decifrarCadena("5,5")).toEqual([[5,5]]);
+  });
+  it("deberia reconocer los numeros de un string con otro separador distintos", () => {
+    expect(decifrarCadena("5/1")).toEqual([5,"1"]);
   });
 
 });
